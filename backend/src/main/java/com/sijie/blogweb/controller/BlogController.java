811570efc34fc829e6fa91341c86c109188c0e50
Jason Yu <yusj.jason@gmail.com>
2021-04-29 12:52:56 +0800

    add web mvc config

--- backend/src/main/java/com/sijie/blogweb/controller/BlogController.java
+++ backend/src/main/java/com/sijie/blogweb/controller/BlogController.java
@@ -3,19 +3,15 @@ package com.sijie.blogweb.controller;
 import com.sijie.blogweb.pojo.Blog;
 import com.sijie.blogweb.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RepositoryRestController
 public class BlogController {
 
     @Autowired
     private BlogRepository blogRepository;
-
-    @GetMapping("/blogtest")
-    public List<Blog> getAllBlogs() {
-        return (List<Blog>) blogRepository.findAll();
-    }
 }

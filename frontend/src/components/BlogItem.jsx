8f9f551933195dcff0d78034439a40bf00dbca57
Jason Yu <yusj.jason@gmail.com>
2021-05-16 21:15:01 +0800

    update BlogItem UI

--- frontend/src/components/BlogItem.jsx
+++ frontend/src/components/BlogItem.jsx
@@ -1,78 +1,75 @@
 // reference: https://bit.dev/orderdesk/prototype/blog-item/~code#index.js
 import "./style.scss";
 
-// blog structure
+// BlogItem only displays the following fields in `props.blog`
 //
-// const defaultPost = {
+// props.blog = {
 //   imgSrc: "",
-//   tags : [
+//   tags: [
 //     {
 //       name: "",
-//       bgColor: ""
-//     },
-//     {
-//       name: "",
-//       bgColor: ""
-//     },
-//   ],
+//       bgColor: ""  (optional)
+//     }
+//    ... ],
 //   date: "",
 //   title: "",
 //   text: "",
 //   url: ""
 // }
 
+// default color for tags
 const colorsInHex = [
-    "#bada55",
-    "#FC6042",
-    "#0ff1ce",
-    "#7fe5f0",
-    "#ffc0cb",
-    "#ffd700",
-    "#d3ffce",
-    "#fcb941",
+  "#bada55",
+  "#FC6042",
+  "#0ff1ce",
+  "#7fe5f0",
+  "#ffc0cb",
+  "#ffd700",
+  "#d3ffce",
+  "#fcb941",
 ]
 
 const TagList = (props) => {
-    return (
-        <div className="blog-item__type-container">
-            {
-            props.tags?.length && props.tags.map((type,index)=>(
-                <div 
-                key={index.toString()} 
-                className="chip" 
-                style={{
-                    backgroundColor: type.bgColor? type.bgColor : colorsInHex[index]
-                }}
-                >
-                <span> { type.name } </span>
-                </div>
-            ))
-            }
-        </div>
-    );
+  return (
+    <div className="blog-item__type-container">
+        {
+        props.tags?.length && props.tags.map((type,index)=>(
+          <div key={index.toString()} className="chip" style={{
+            backgroundColor: type.bgColor? type.bgColor : colorsInHex[index]
+          }} >
+            <span> { type.name } </span>
+          </div>
+        ))
+        }
+    </div>
+  );
 }
 
 const BlogItem = (props) => {
   return ( 
     <div className="blog-item__container">
       <div className="blog-item__inner">
-         <div className="blog-item__image-container">
-            <img src={props.blog.imgSrc}/>
-         </div>
-         <div className="blog-item__content">
-            <TagList tags={props.blog.tags} />
+        <div className="blog-item__image-container">
+          <img src={props.blog.imgSrc}/>
+        </div>
 
+        <div className="blog-item__content">
+          <h2 className="blog-item__title">{props.blog.title}</h2>
+          <p className="blog-item__text">
+            {props.blog.text}
+          </p>
+          <TagList tags={props.blog.tags} />
+
+          <div className="blog-item__bottom-container">
             <span className="blog-item__date">{props.blog.date}</span>
-            <h2 className="blog-item__title">{props.blog.title}</h2>
-            <p className="blog-item__text">
-              {props.blog.text}
-            </p>
             <div className="blog-item__read-more__container">
-                <a href={props.blog.url} className="read-more">
-                  READ MORE
-                </a>
+              <a href={props.blog.url} className="read-more">
+                READ MORE
+              </a>
             </div>
-         </div>
+          </div>
+        </div>
+
       </div>
     </div>
    );
--- frontend/src/reducers/blogReducer.jsx
+++ frontend/src/reducers/blogReducer.jsx
@@ -1,22 +1,22 @@
 const { act } = require("react-dom/test-utils");
 
 const initialState = {
-    blogs: [],
-    currentBlog: {},
-    count: 0
+  blogs: [],
+  currentBlog: {},
+  count: 0
 }
 
 const blogReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "FOO_BLOG":
-            state = {
-                ...state,
-                currentBlog: action.blog
-            }
-            break;
-    }
+  switch (action.type) {
+    case "FOO_BLOG":
+      state = {
+        ...state,
+        currentBlog: action.blog
+      }
+      break;
+  }
 
-    return state;
+  return state;
 } 
 
 export default blogReducer;

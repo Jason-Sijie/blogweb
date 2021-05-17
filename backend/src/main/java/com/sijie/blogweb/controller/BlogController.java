2358584c2a46f246b80726dd327cc7cdcb0ed6ad
Jason Yu <yusj.jason@gmail.com>
2021-05-17 14:06:16 +0800

    1. allow all origins to access blog api

--- backend/src/main/java/com/sijie/blogweb/controller/BlogController.java
+++ backend/src/main/java/com/sijie/blogweb/controller/BlogController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-
+@CrossOrigin(origins = "*")
 @RepositoryRestController
 @RequestMapping(value = "/blog")
 public class BlogController {
--- backend/src/main/java/com/sijie/blogweb/repository/BlogRepository.java
+++ backend/src/main/java/com/sijie/blogweb/repository/BlogRepository.java
@@ -4,6 +4,7 @@ import com.sijie.blogweb.pojo.Blog;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
